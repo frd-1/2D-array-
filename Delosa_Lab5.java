aabda40afeda9c32d446cba0b9cf9fc7a3c1365a
fr <243729514+frd-1@users.noreply.github.com>
2026-04-07 15:12:35 +0800

    Add files via upload

---
+++ Delosa_Lab5.java
@@ -0,0 +1,79 @@
+package Delosa;
+import java.util.*;
+
+public class Delosa_Lab5 {
+	
+	//Hotel Reservation System (Java, 2D Array)
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		int hotel[][] = new int[7][5]; //7 floors, 5 rooms per floor
+		int choice = 0;
+		
+		while (choice != 4 ) {
+		// display menu
+			System.out.println("=== HOTEL RESERVATION SYSTEM ===");
+			System.out.println("1. View Rooms");
+			System.out.println("2. Check in");
+			System.out.println("3. Check out");
+			System.out.println("4. Exit");
+			System.out.print("Enter your choice: ");
+			choice = sc.nextInt();		
+			
+			switch (choice) {
+			//View rooms
+	        case 1: // View Rooms
+                for (int i = 0; i < 7; i++) {
+                    System.out.print("Floor " + (i + 1) + ": ");
+                    for (int j = 0; j < 5; j++) {
+                        System.out.print("[" + hotel[i][j] + "]");
+                    }
+                    System.out.println();
+                }
+                break;
+
+			//Check in
+			case 2:
+				System.out.print("Enter floor (1-7): ");
+				int floor_In = sc.nextInt() - 1;
+				System.out.println("Enter room (1-5): ");
+				int room_In = sc.nextInt() - 1;
+				
+				if (hotel [floor_In][room_In] == 0) {
+					hotel [floor_In][room_In] = 1;
+					System.out.println("Check-in Successful!");
+				} else {
+					System.out.println("Room already occupied!");
+				}
+				break;
+				
+				//check out
+			case 3:
+				System.out.print("Enter floor (1-7): ");
+				int floor_Out = sc.nextInt() - 1;
+				System.out.println("Enter room (1-5): ");
+				int room_Out = sc.nextInt() - 1;
+				
+				if (hotel [floor_Out][room_Out] == 1) {
+					hotel [floor_Out][room_Out] = 0;
+					System.out.println("Check-out Successful!");
+				} else {
+					System.out.println("Room already empty!");
+				}
+				break;
+				
+			//exit	
+			case 4:
+				System.out.println("Exiting system. Goodbye!");
+				break;
+				
+				default:
+					System.out.println("Invalid choice. Try again!");
+			}
+			System.out.println();
+		}
+		sc.close();	
+	}
+}
+
